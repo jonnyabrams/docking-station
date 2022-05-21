aa11a088e6931b98501942f349029ef62d4adbbc
Jonny Abrams <jonnyabrams@hotmail.com>
2022-05-21 10:33:50 +0100

    Added dock function

--- DockingStation.js
+++ DockingStation.js
@@ -3,6 +3,10 @@ class DockingStation {
   constructor() {
     this.bikes = [];
   };
+
+  dock(bike) {
+    this.bikes.push(bike);
+  }
 };
 
 module.exports = DockingStation;
--- DockingStation.test.js
+++ DockingStation.test.js
@@ -2,6 +2,8 @@ const DockingStation = require('./DockingStation')
 
 describe('DockingStation', () => {
   const station = new DockingStation;
+  const bike1 = { working: true }
+  const bike2 = { working: false }
 
   it('is an instance of the DockingStation class', () => {
     expect(station).toBeInstanceOf(DockingStation);
@@ -9,5 +11,10 @@ describe('DockingStation', () => {
 
   it('is constructed with an array for storing bikes', () => {
     expect(station.bikes).toEqual([]);
+  });
+
+  it('can dock a bike', () => {
+    station.dock(bike1);
+    expect(station.bikes.length).toEqual(1);
   });
 });
